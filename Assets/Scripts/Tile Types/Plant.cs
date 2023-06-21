56331ab151a65b34240a6406e93b90cb1611156e
ConnorMcGrathMQ <69331425+ConnorMcGrathMQ@users.noreply.github.com>
2023-06-21 12:37:49 +1000

    Null handling

--- Assets/Scripts/Tile Types/Plant.cs
+++ Assets/Scripts/Tile Types/Plant.cs
@@ -15,24 +15,28 @@ public class Plant : Tile
     void Start()
     {
         Plant prev = FindSmallestNeighbour();
-        if (prev.pos.x > this.pos.x) {
-            //Prev on Rightside
-            this.inDir = Dir.Right;
-            prev.outDir = Dir.Left;
-        } else if (prev.pos.x < this.pos.x) {
-            //Prev on Leftside
-            this.inDir = Dir.Left;
-            prev.outDir = Dir.Right;
-        } else if (prev.pos.y > this.pos.y) {
-            //Prev on Top
-            this.inDir = Dir.Up;
-            prev.outDir = Dir.Down;
-        } else if (prev.pos.y < this.pos.y) {
-            //Prev on Bottom
-            this.inDir = Dir.Down;
-            prev.outDir = Dir.Up;
+        if (prev != null) {
+            if (prev.pos.x > this.pos.x) {
+                //Prev on Rightside
+                this.inDir = Dir.Right;
+                prev.outDir = Dir.Left;
+            } else if (prev.pos.x < this.pos.x) {
+                //Prev on Leftside
+                this.inDir = Dir.Left;
+                prev.outDir = Dir.Right;
+            } else if (prev.pos.y > this.pos.y) {
+                //Prev on Top
+                this.inDir = Dir.Up;
+                prev.outDir = Dir.Down;
+            } else if (prev.pos.y < this.pos.y) {
+                //Prev on Bottom
+                this.inDir = Dir.Down;
+                prev.outDir = Dir.Up;
+            }
+            remainingDist = prev.remainingDist - 1;
+        } else {
+            //PLANT START POINT STUFF GOES HERE
         }
-        remainingDist = prev.remainingDist - 1;
     }
 
     // Update is called once per frame
@@ -47,7 +51,9 @@ public class Plant : Tile
         foreach (Tile t in Tiles)
         {
             if (t is Plant p) {
-                plantList.Add(p);
+                if (p.species == this.species) {
+                    plantList.Add(p);
+                }
             }
         }
         Plant smallest = null;
